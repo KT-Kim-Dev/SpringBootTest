6a966be097b770bbbe91ebdaa29c1aa93e6025a6
winneris1234 <winneris1234@gmail.com>
2021-05-29 23:23:46 +0900

    spring test

--- createProjectSample-1/src/main/resources/schema.sql
+++ createProjectSample-1/src/main/resources/schema.sql
@@ -2,10 +2,11 @@ DROP TABLE IF EXISTS accounts;
  
 CREATE TABLE accounts (
   id INT AUTO_INCREMENT  PRIMARY KEY,
-  email VARCHAR(255) NOT NULL
+  email VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL
 );
  
-INSERT INTO accounts (email) VALUES
-  ('email1@email.com'),
-  ('email2@email.com'),
-  ('email3@email.com');
+INSERT INTO accounts (email, name) VALUES
+  ('email1@email.com', 'Elise'),
+  ('email2@email.com', 'John'),
+  ('email3@email.com', 'Mike;s);
