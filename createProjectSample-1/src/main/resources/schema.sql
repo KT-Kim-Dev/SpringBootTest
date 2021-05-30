c0c3d40a785d12b8365d2137919445ea084c6840
gelix-kyoungtae <winneris1234@gmail.com>
2021-05-30 13:12:01 +0900

    프로그래머스 문제 해결 - SQL 입양사각 구하기 2

--- createProjectSample-1/src/main/resources/schema.sql
+++ createProjectSample-1/src/main/resources/schema.sql
@@ -10,3 +10,13 @@ INSERT INTO accounts (email, name) VALUES
   ('email1@email.com', 'Elise'),
   ('email2@email.com', 'John'),
   ('email3@email.com', 'Mike;s);
+  
+ -- 입양 시각 구하기 2
+ WITH RECURSIVE TIME 
+ AS( SELECT 0 AS H UNION ALL
+ 	SELECT H+1 FROM TIME WHERE H < 23
+ )
+ SELECT H, COUNT(HOUR(DATETIME)) AS COUNT
+ FROM TIME LEFT	OUTER JOIN ANIMAL_OUTS
+ ON H=HOUR(DATETIME)
+ GROUP BY H;
